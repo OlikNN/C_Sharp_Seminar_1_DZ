52180221c4b69bfa04eb9d7b65af19fb5418a03b
OlikNN <oliknn@gmail.com>
2022-11-29 21:13:52 +0300

    убрала проверку на четность

--- task_8/Program.cs
+++ task_8/Program.cs
@@ -6,7 +6,7 @@
 Console.WriteLine("Введите число");
 int number = Convert.ToInt32(Console.ReadLine());
 
-for (int i = 0; i <= number; i += 2)
+for (int i = 2; i <= number; i += 2)
 {
         Console.Write(i + " ");
 }
